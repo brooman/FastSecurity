b9a1a71bf782308cfc4131aaba9a4722c4cb14a1
laykith <laykith@gmail.com>
2018-11-07 14:11:04 +0100

    Removed Default Vue Scaffolding & Added global CSS properties

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,54 @@
 <template>
   <div id="app">
     <Navbar />
-    <img 
-      alt="Vue logo" 
-      src="./assets/logo.png"
-    >
-    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     Navbar
   }
 }
 </script>
 
 <style>
+@font-face {
+    font-family: "AG-Regular";
+    src: url(assets/fonts/AktivGrotesk-Regular.otf)
+}
+@font-face {
+    font-family: "AG-Light";
+    src: url(assets/fonts/AktivGrotesk-Light.otf)
+}
+@font-face {
+    font-family: "AG-Bold";
+    src: url(assets/fonts/AktivGrotesk-Bold.otf)
+}
+@font-face {
+    font-family: "AG-Medium";
+    src: url(assets/fonts/AktivGrotesk-Medium.otf)
+}
+
+* {
+  font-family: 'AG-Medium';
+  color: oldlace;
+}
+
 body {
-  margin: 0px;
+  margin: 0;
+  padding: 0;
+  background: #333;
 }
 #app {
-  margin: 0px;
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'AG-Bold', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  height: 4000px; /* Temporary for seeing scrolling */ 
+  overflow: hidden;
 }
 </style>
