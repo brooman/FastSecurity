5b11e7bddf0a18fd8ee42480b821973ccfa632da
laykith <laykith@gmail.com>
2018-11-12 14:09:28 +0100

    Removed Exterior / HolderComponent

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,18 @@
   <div id="app">
     <Navbar />
     <HeroImg />
-    <Exterior />
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
 import HeroImg from './components/HeroImg.vue'
-import Exterior from './components/Exterior.vue'
 
 export default {
   name: 'App',
   components: {
     Navbar,
     HeroImg,
-    Exterior,
   }
 }
 </script>
