a6cb2b4f1bfb62412d9d232620311240097226e3
laykith <laykith@gmail.com>
2018-11-14 11:45:23 +0100

    Updated Global CSS and Added HeroText Component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <Navbar />
     <HeroImg />
+    <HeroText />
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
 import HeroImg from './components/HeroImg.vue'
+import HeroText from './components/HeroText.vue'
 
 export default {
   name: 'App',
   components: {
     Navbar,
     HeroImg,
+    HeroText,
   }
 }
 </script>
@@ -45,7 +48,7 @@ export default {
 body {
   margin: 0;
   padding: 0;
-  background: #333;
+  background: #111;
 }
 #app {
   font-family: 'AG-Bold', sans-serif;
@@ -54,5 +57,11 @@ body {
   text-align: center;
   height: 4000px; /* Temporary for seeing scrolling */ 
   overflow: hidden;
+}
+
+.break {
+  height: 5px;
+  width: 40%;
+  background: linear-gradient(to right, cyan 0%, cyan 50%, blue 50%, blue 100%);
 }
 </style>
